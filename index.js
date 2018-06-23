dd9dc2d0f417e6f3b0df42c841262539d106c10e
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-06-23 21:02:29 +0200

    Adds leaderboard routes ans saving.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict'
 const path = require('path')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const keys = require('./keys')
 const questions = require('./questions')
+const Record = require('./record')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000
+
+mongoose.connect(keys.mongo)
+.then(result => {
+  console.log("Connected to database");
+})
+.catch(err => {
+  console.error(err);
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -28,6 +42,39 @@ app.get('/answer/:id', (req, res) => {
   }
   const key = Object.keys(questions[id])[0]
   return res.json(questions[id][key])
+})
+
+app.get('/leaderboard', (req, res) => {
+  Record.find()
+  .then(data => {
+    res.json(data.map(el => {
+      return {
+        date: el.date,
+        correct: el.correct,
+        questions: el.questions,
+        nickname: el.nickname
+      }
+    }))
+  })
+  .catch(err => {
+    console.error(err)
+  })
+})
+
+app.post('/leaderboard', (req, res) => {
+  console.log(req.body)
+  const newRecord = new Record({
+    nickname: req.body.nickname,
+    correct: req.body.correct,
+    questions: req.body.questions
+  })
+  newRecord.save()
+  .then(result => {
+    return res.sendStatus(200)
+  })
+  .catch(err => {
+    console.error(err)
+  })
 })
 
 app.listen(PORT, () => {
